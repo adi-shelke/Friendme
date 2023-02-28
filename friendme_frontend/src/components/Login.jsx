827e3d16a62d454d1722a6f98342d4e115393c77
Adinath Shelke <shelkeadinath3@gmail.com>
2023-03-01 00:29:44 +0530

    added the gAuth to the login page but it is deprecated

--- friendme_frontend/src/components/Login.jsx
+++ friendme_frontend/src/components/Login.jsx
@@ -4,7 +4,12 @@ import { useNavigate } from 'react-router-dom'
 import loginVideo from "../assets/share.mp4"
 import logo from '../assets/logowhite.png'
 import {FcGoogle} from 'react-icons/fc'
+import { useEffect } from 'react'
 const Login = () => {
+  
+  const responseGoogle=(response)=>{
+    console.log(response)
+  }
   return (
     <div className='flex justify-start items-center flex-col h-screen'>
       <div className='w-full h-full relative'>
@@ -18,7 +23,27 @@ const Login = () => {
           className='w-full h-full object-cover'
         />
         <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay '>
-          
+          <div className='p-5 '>
+            <img src={logo} alt="logo" width="130px" />
+          </div>
+          <div className='shadow-2xl'>
+            <GoogleLogin 
+            
+              clientId={import.meta.env.VITE_GOOGLE_API_KEY}
+              render={(renderProps)=>(
+                <button
+                  type='button'
+                  className='bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none'     
+                  onClick={renderProps.onClick}    
+                  disabled={renderProps.disabled}       >
+                  <FcGoogle className='mr-4'/>Sign in with Google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy="single_host_origin"
+            />
+          </div>
         </div>
       </div>
     </div>
