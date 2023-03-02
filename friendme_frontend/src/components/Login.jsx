0526595c33f7665d6883bd9313631735a30ad1a7
Adinath Shelke <shelkeadinath3@gmail.com>
2023-03-03 00:00:38 +0530

    Added google authentication system

--- friendme_frontend/src/components/Login.jsx
+++ friendme_frontend/src/components/Login.jsx
@@ -1,34 +1,45 @@
-import React from 'react'
-import GoogleLogin from "react-google-login"
-import { useNavigate } from 'react-router-dom'
-import loginVideo from "../assets/share.mp4"
-import logo from '../assets/logowhite.png'
-import {FcGoogle} from 'react-icons/fc'
-import { useEffect } from 'react'
+import React from "react";
+// import GoogleLogin from "react-google-login";    DEPRICATED
+import { useNavigate } from "react-router-dom";
+import loginVideo from "../assets/share.mp4";
+import logo from "../assets/logowhite.png";
+// import { FcGoogle } from "react-icons/fc";
+import { GoogleLogin } from "@react-oauth/google";
+import jwt_decode from "jwt-decode";
+
 const Login = () => {
-  
-  const responseGoogle=(response)=>{
-    console.log(response)
-  }
+  const responseGoogle = (response) => {
+    const decodedToken = jwt_decode(response.credential);
+    console.log(decodedToken)
+    localStorage.setItem('user',JSON.stringify(decodedToken))
+    const {name, sub, picture} =decodedToken
+    console.log(picture)
+
+
+  };
   return (
-    <div className='flex justify-start items-center flex-col h-screen'>
-      <div className='w-full h-full relative'>
-        <video 
+    <div className="flex justify-start items-center flex-col h-screen">
+      <div className="w-full h-full relative">
+        <video
           src={loginVideo}
           type="video/mp4"
-          loop 
-          controls={false} 
+          loop
+          controls={false}
           muted
           autoPlay
-          className='w-full h-full object-cover'
+          className="w-full h-full object-cover"
         />
-        <div className='absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay '>
-          <div className='p-5 '>
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay ">
+          <div className="p-5 ">
             <img src={logo} alt="logo" width="130px" />
           </div>
-          <div className='shadow-2xl'>
-            <GoogleLogin 
-            
+          <div className="shadow-2xl">
+
+
+            {/* DEPRICATED */}
+
+
+            {/* <GoogleLogin 
               clientId={import.meta.env.VITE_GOOGLE_API_KEY}
               render={(renderProps)=>(
                 <button
@@ -42,12 +53,23 @@ const Login = () => {
               onSuccess={responseGoogle}
               onFailure={responseGoogle}
               cookiePolicy="single_host_origin"
+            /> */}
+
+
+            {/* NEW METHOD OF GOOGLE AUTH */}
+            <GoogleLogin
+            className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+              onSuccess={responseGoogle}
+              onError={() => {
+                console.log("Login Failed");
+              }}
             />
+            ;
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- friendme_frontend/src/main.jsx
+++ friendme_frontend/src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_API_KEY}>
+        <App />
+      </GoogleOAuthProvider>
     </Router>
   </React.StrictMode>
 );
